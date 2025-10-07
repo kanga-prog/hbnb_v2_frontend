a8cbdedb75f9b1de09af34a5e03386143f07d12f
kanga-prog <9952@holbertonstudents.com>
2025-10-07 14:27:11 +0200

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL|| "/api",
+  baseURL: process.env.REACT_APP_API_URL|| "https://hbnbv2backend-production.up.railway.app/api",
 });
 
 // Ajouter automatiquement le token à chaque requête
